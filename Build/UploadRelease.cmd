3a6fc51f89d7d4e1dd537e281c1353772dc521d0
objo <objo@OSLWP7783.verit.dnv.com>
2012-06-11 13:22:46 +0200

    Automated build

---
+++ Build/UploadRelease.cmd
@@ -0,0 +1,1 @@
+..\Tools\CodeplexReleaseUploader.exe /UserName=%CODEPLEX_USERNAME% /Password=%CODEPLEX_PASSWORD% /Project=usagestats "/Release=Version 2012.1" "/Upload=..\Output\usagestats.msi,UsageStats-%1.msi" > UploadRelease.log
---
+++ Build/updateversion.cmd
@@ -0,0 +1,2 @@
+..\Tools\versionUpdater.exe /Company="objo.net" /Copyright="Copyright (C) objo 2012." /Directory=".." /Version=%1 > UpdateVersion.log
+
