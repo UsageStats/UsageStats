0eba5133ba9773e136d3cb8d47dfa91d12c0730f
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-12-30 00:22:04 +0100

    Remove obsolete tools folder

--- Tools/CleanSource.cmd
+++
@@ -1,2 +0,0 @@
-Lynx\CleanSource.exe /cleansummary /cleanregions ..\Source > CleanSource.log
-Lynx\UpdateFileHeaders.exe /company=Glissando /copyright-file=..\License.txt ..\Source > UpdateFileHeaders.log
